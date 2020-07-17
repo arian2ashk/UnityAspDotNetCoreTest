bf19c8b1906de3da22cbdeb726aab2c7470fa230
arian ashk <akadkhoda@configit.com>
2020-07-18 00:47:29 +0200

    initial commit

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Unity;
+using Unity.Lifetime;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void ConfigureContainer(IUnityContainer container)
+        {
+            var webHostEnvironment = container.Resolve<IWebHostEnvironment>();
+            Debug.Assert(webHostEnvironment != null, "webHostEnvironment is null");
+
+            container.RegisterFactory<ITest>(c =>
+            {
+                var webHost = c.Resolve<IWebHostEnvironment>();
+                return new Test(webHost.ContentRootPath);
+            }, new SingletonLifetimeManager());
+
+            var test = container.Resolve<ITest>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ WebApplication1/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public interface ITest
+    {
+        string Path { get; }
+    }
+
+    public class Test : ITest
+    {
+        public string Path { get; }
+
+        public Test(string path)
+        {
+            Path = path;
+        }
+    }
+}
